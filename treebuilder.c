6a1da004055ece662f062ca843c1a4bb7a60bd96
Mikael Teemu Heikkilä <34525164+deadwife@users.noreply.github.com>
2017-12-14 16:02:10 +0300

    Update treebuilder.c

--- treebuilder.c
+++ treebuilder.c
@@ -11,20 +11,23 @@
 #define S sleep(999);
 #define BUFSIZE 8096
 
-Tree * exp0()
+Tree * exp0(char * cur_str)
 {
-     char cur_str[BUFSIZE];
      Lex code;
      Tree * Crop = NULL,* Crop0 = NULL;
-     Crop = expr();
-     while ((code = nextlex(cur_str)) == _background)
+     Crop = expr(&code, cur_str);
+     if (code == _zero)
+          code = nextlex(cur_str);
+     while (code == _background)
      {
           Crop0 = (Tree *) calloc (1, sizeof(Tree));
           Crop0->code = code;
           Crop0->args = NULL;
           Crop0->left = Crop;
-          Crop0->right = expr();
+          Crop0->right = expr(&code, cur_str);
           Crop = Crop0;
+          if (code == _zero)
+               code = nextlex(cur_str);
      }
      if (cur_str[0] != '\0')
      {
@@ -38,20 +41,22 @@ Tree * exp0()
 /////////////////////////////////////////////////////////////////////
 
 
-Tree * expr()
+Tree * expr(Lex * code, char * cur_str)
 {
-     char cur_str[BUFSIZE];
-     Lex code;
      Tree * Crop = NULL,* Crop0 = NULL;
-     Crop = item();
-     while (((code = nextlex(cur_str)) == _and) || (code == _or))
+     Crop = item(code, cur_str);
+     if (*code == _zero)
+          *code = nextlex(cur_str);
+     while ((*code == _and) || (*code == _or))
      {
           Crop0 = (Tree *) calloc (1, sizeof(Tree));
-          Crop0->code = code;
+          Crop0->code = *code;
           Crop0->args = NULL;
           Crop0->left = Crop;
-          Crop0->right = item();
+          Crop0->right = item(code, cur_str);
           Crop = Crop0;
+          if (*code == _zero)
+               *code = nextlex(cur_str);
      }
      return Crop;
 }
@@ -60,20 +65,22 @@ Tree * expr()
 /////////////////////////////////////////////////////////////////////
 
 
-Tree * item()
+Tree * item(Lex * code, char * cur_str)
 {
-     char cur_str[BUFSIZE];
-     Lex code;
      Tree * Crop = NULL,* Crop0 = NULL;
-     Crop = head();
-     while ((code = nextlex(cur_str)) == _pipe)
+     Crop = head(code, cur_str);
+     if (*code == _zero)
+          *code = nextlex(cur_str);
+     while (*code == _pipe)
      {
           Crop0 = (Tree *) calloc (1, sizeof(Tree));
-          Crop0->code = code;
+          Crop0->code = *code;
           Crop0->args = NULL;
           Crop0->left = Crop;
-          Crop0->right = head();
+          Crop0->right = head(code, cur_str);
           Crop = Crop0;
+          if (*code == _zero)
+               *code = nextlex(cur_str);
      }
      return Crop;
 }
@@ -82,26 +89,31 @@ Tree * item()
 /////////////////////////////////////////////////////////////////////
 
 
-Tree * head()
+Tree * head(Lex * code, char * cur_str)
 {
-     char cur_str[BUFSIZE];
-     Lex code;
      Tree * Crop = NULL,* Crop0 = NULL;
-     Crop = mult();
-     while ((code = nextlex(cur_str)) == _redirect)
+     Crop = mult(code, cur_str);
+     if (*code == _zero)
+          *code = nextlex(cur_str);
+     while (*code == _redirect)
      {
           Crop0 = (Tree *) calloc (1, sizeof(Tree));
-          Crop0->code = code;
-          if ((code = nextlex(cur_str)) == _arg)
+          Crop0->code = *code;
+          if ((*code = nextlex(cur_str)) == _arg)
+          {
                Crop0->args = cur_str;
+               Crop0->left = Crop;
+               Crop0->right = mult(code, cur_str);
+               Crop = Crop0;
+               if (*code == _zero)
+                    *code = nextlex(cur_str);
+          }
           else
           {
                error("in head - no args - sleeping...\n");
                S
           }
-          Crop0->left = Crop;
-          Crop0->right = mult();
-          Crop = Crop0;
+
      }
      return Crop;
 }
@@ -110,21 +122,22 @@ Tree * head()
 /////////////////////////////////////////////////////////////////////
 
 
-Tree * mult()
+Tree * mult(Lex * code, char * cur_str)
 {
-     char cur_str[BUFSIZE];
-     Lex code;
      Tree * Crop = NULL;
      Crop = (Tree *) calloc (1, sizeof(Tree));
      Crop->left = NULL;
      Crop->right = NULL;
-     if((code = nextlex(cur_str)) == _arg)
-          Crop->code = _arg;
+     if((*code = nextlex(cur_str)) == _arg)
+     {
+          Crop->code = *code;
+          Crop->args = cur_str;
+          *code = _zero;
+     }
      else
      {
           error("in mult - UNKNOWN LEX - sleeping...\n");
           S
      }
-     Crop->args = cur_str;
      return Crop;
 }
