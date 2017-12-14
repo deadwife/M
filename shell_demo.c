458f2e4888a1fc23c1bc14dc102782845ac7a13e
Mikael Teemu Heikkilä <34525164+deadwife@users.noreply.github.com>
2017-12-14 16:38:18 +0300

    Update shell_demo.c

--- shell_demo.c
+++ shell_demo.c
@@ -9,18 +9,21 @@
 #include "head/my_funcs.h"
 #include "head/my_tps.h"
 #define CLS system("clear");
+#define BUFSIZE 8096
 
 int main()
 {
      Tree * Tree;
+     char cur_str[BUFSIZE];
+     char * demolished_args[BUFSIZE];
 Begin:
 
      write(1,">: ", 3);
      Tree = NULL;
      //
-     Tree = exp0(); //Grow a Tree from a Crop
+     Tree = exp0(cur_str); //Grow a Tree from a Crop
      //
-     executor2000(Tree); //Do stuff
+     executor2000(Tree, demolished_args); //Do stuff
      //
      lumberjack3000(Tree); //Free a tree
      //
